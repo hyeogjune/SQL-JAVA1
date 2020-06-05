a9abb4c549b2193a054eecccefc8498583ad0c32
jang hyeogjune <jh870218@naver.con>
2020-06-05 16:20:49 +0900

    add.Ex1.java

---
+++ Ex2.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Ex2 {
+	public static void main (String[] args) {
+		
+		int numOfApples = 123;
+		int sizeOfBucket = 10;
+		int numOfBucket = ((numOfApples / sizeOfBucket) + 1);
+		System.out.println("필요한 바구니의 수 : " + numOfBucket);
+	}
+}
